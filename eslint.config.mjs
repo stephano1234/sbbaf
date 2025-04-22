1de2f82ac6b8b92a8cc2d268cb819d29c0492a2d
Stephano Margarita La Regina <stephano.1234@hotmail.com>
2025-04-22 15:32:05 +0100

    test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,46 +1,25 @@
+import { includeIgnoreFile } from "@eslint/compat";
 import css from "@eslint/css";
 import js from "@eslint/js";
-import json from "@eslint/json";
-import markdown from "@eslint/markdown";
 import { defineConfig } from "eslint/config";
 import globals from "globals";
+import { fileURLToPath } from "node:url";
 import tseslint from "typescript-eslint";
 
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
+
 export default defineConfig([
+  includeIgnoreFile(gitignorePath),
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
   {
     files: ["**/*.{js,mjs,cjs,ts}"],
     plugins: { js },
     extends: ["js/recommended"],
   },
-  {
-    files: ["**/*.{js,mjs,cjs,ts}"],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
-  },
   tseslint.configs.recommended,
-  {
-    files: ["**/*.json"],
-    plugins: { json },
-    language: "json/json",
-    extends: ["json/recommended"],
-  },
-  {
-    files: ["**/*.jsonc"],
-    plugins: { json },
-    language: "json/jsonc",
-    extends: ["json/recommended"],
-  },
-  {
-    files: ["**/*.json5"],
-    plugins: { json },
-    language: "json/json5",
-    extends: ["json/recommended"],
-  },
-  {
-    files: ["**/*.md"],
-    plugins: { markdown },
-    language: "markdown/commonmark",
-    extends: ["markdown/recommended"],
-  },
   {
     files: ["**/*.css"],
     plugins: { css },
--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,8 +1,4 @@
 export default {
-  "**/!(package-lock)*.{js,mjs,cjs,ts,json,jsonc,json5,md,css}": [
-    "eslint",
-    "prettier --write --ignore-unknown",
-  ],
-  "**/!(*.{js,mjs,cjs,ts,json,jsonc,json5,md,css})":
-    "prettier --write --ignore-unknown",
+  "**/*.{js,mjs,cjs,ts,css}": ["eslint", "prettier --write --ignore-unknown"],
+  "**/*.!({js,mjs,cjs,ts,css})": "prettier --write --ignore-unknown",
 };
